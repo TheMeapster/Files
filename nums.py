4ad79488ae9ef0d05e6850fac6fba38dcc4cca49
Dylan <dylan@Hexer-pro-XD.local>
2018-03-14 13:15:01 -0400

    bottom text

--- nums.py
+++ nums.py
@@ -10,18 +10,12 @@ def main():
     for i in range(len(A)):
         A[i] = int(A[i])
     A.sort()
-    print "The average of numsText.txt is: ", A[len(A)/2]
     count = 0
-    finalCount = 0
     mostCommon = 0
     for i in range(1,len(A)):
-        if A[i] == A[i-1]:
-            count += 1
-        else:
-            if count > finalCount:
-                finalCount = count
-                mostCommon = A[i]
-            count = 1
+        if i.count(A):
+            count = i.count(A)
+            mostCommon = i
     print A
     print "The most common number in numsText.txt: ", mostCommon, "and this number repeats", finalCount, "times"
 main()
