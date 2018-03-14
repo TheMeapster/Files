6f667ae7f10abf72d4b74897ab3976136bee8705
Dylan <dylan@Hexer-pro-XD.local>
2018-03-14 13:42:23 -0400

    number 1 done

--- dick.py
+++ dick.py
@@ -5,4 +5,12 @@
 import os
 
 def main():
-    f = open (dick.txt)
+    f=open("dick.txt","r")
+    A = f.readlines()
+    f.close()
+    fiveCount = []
+    for i in A:
+        if len(i) == 5:
+            fiveCount.append(i)
+    print "The number of five letter words is:", len(fiveCount)
+main()
