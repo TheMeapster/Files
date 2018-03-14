1ad3d53d1e1418840bf1e8e2e667d696a8646587
Shanthi Sriram <nikeetasriram@Shanthis-MacBook-Air.local>
2018-03-14 13:32:33 -0400

    mother ick lol d

--- dick.py
+++ dick.py
@@ -1,1 +1,8 @@
+#Files/Dick
+#Dylan and Avi
+#3/15/18
 
+import os
+
+def main():
+    f = open (dick.txt)
--- nums.py
+++ nums.py
@@ -1,4 +1,4 @@
-#Files/nums
+l#Files/nums
 #Dylan and Avi
 #2/28/18
 import os
@@ -13,14 +13,11 @@ def main():
     count = 0
     mostCommon = 0
     divs31 = []
-    for i in A:
-        if A.count(i) > count:
-            count = A.count(i)
+    for i in range(1,len(A)):
+        if i.count(A):
+            count = i.count(A)
             mostCommon = i
-        if i%31 == 0:
+        if%31==0:
             divs31.append(i)
-    divs31 = list(set(divs31))
-    divs31.sort()
-    print "The most common number in numsText.txt", mostCommon, "and this number repeats", count, "times."
-    print "The numbers that are divisible by 31 are", divs31
+    print "The most common number in numsText.txt: ", mostCommon, "and this number repeats", finalCount, "times"
 main()
