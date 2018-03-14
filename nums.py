7228b87046f5349eb075f4f7c55846229a665ff6
Dylan <dylan@Hexer-pro-XD.local>
2018-03-14 13:26:59 -0400

    kell ur selv

--- nums.py
+++ nums.py
@@ -12,10 +12,15 @@ def main():
     A.sort()
     count = 0
     mostCommon = 0
-    for i in range(1,len(A)):
-        if i.count(A):
-            count = i.count(A)
+    divs31 = []
+    for i in A:
+        if A.count(i) > count:
+            count = A.count(i)
             mostCommon = i
-    print A
-    print "The most common number in numsText.txt: ", mostCommon, "and this number repeats", finalCount, "times"
+        if i%31 == 0:
+            divs31.append(i)
+    divs31 = list(set(divs31))
+    divs31.sort()
+    print "The most common number in numsText.txt", mostCommon, "and this number repeats", count, "times."
+    print "The numbers that are divisible by 31 are", divs31
 main()
