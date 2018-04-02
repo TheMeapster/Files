a7d44ae4ab5fa65f5eeffd9fe10acd81f8f8f35d
Dylan <dylan@Hexer-pro-XD.local>
2018-04-02 13:40:31 -0400

    working

--- dick.py
+++ dick.py
@@ -8,9 +8,30 @@ def main():
     f=open("dick.txt","r")
     A = f.readlines()
     f.close()
-    fiveCount = []
+    fives = []
     for i in A:
-        if len(i) == 5:
-            fiveCount.append(i)
-    print "The number of five letter words is:", len(fiveCount)
+        if len(i) == 6:
+            fives.append(i)
+    print "The number of five letter words is:", len(fives)
+    threeS = []
+    for i in A:
+        sCount = 0
+        for j in i:
+            if j == 's':
+                sCount += 1
+        if sCount == 3:
+            threeS.append(i)
+    print "The number of words with three s's is:", len(threeS)
+    startF = []
+    for i in A:
+        if i[0] == 'f':
+            startF.append(i)
+    print "The number of words that start with F is:", len(startF)
+    palindromes = []
+    for i in fives:
+        print i
+        print i[::-1]
+        if i == i[::-1]:
+            palindromes.append(i)
+
 main()
